d50f19af2b5f32a0be472ffdd8a63afb9c6dffa1
邓昌亮 <dcl@guance.com>
2023-03-03 15:59:58 +0800

    fix: sampleRage to  sessionSampleRate

--- packages/logs/src/domain/logsSessionManager.js
+++ packages/logs/src/domain/logsSessionManager.js
@@ -2,25 +2,28 @@ import { performDraw, startSessionManager } from '@cloudcare/browser-core'
 
 export var LOGS_SESSION_KEY = 'logs'
 
-
-export var LoggerTrackingType =  {
+export var LoggerTrackingType = {
   NOT_TRACKED: '0',
-  TRACKED: '1',
+  TRACKED: '1'
 }
 
 export function startLogsSessionManager(configuration) {
-  var sessionManager = startSessionManager(configuration.cookieOptions, LOGS_SESSION_KEY, function(rawTrackingType) {
-    return computeSessionState(configuration, rawTrackingType)
-  })
+  var sessionManager = startSessionManager(
+    configuration.cookieOptions,
+    LOGS_SESSION_KEY,
+    function (rawTrackingType) {
+      return computeSessionState(configuration, rawTrackingType)
+    }
+  )
   return {
-    findTrackedSession: function(startTime){
+    findTrackedSession: function (startTime) {
       var session = sessionManager.findActiveSession(startTime)
       return session && session.trackingType === LoggerTrackingType.TRACKED
         ? {
-            id: session.id,
+            id: session.id
           }
         : undefined
-    },
+    }
   }
 }
 
@@ -33,20 +36,25 @@ export function startLogsSessionManager(configuration) {
 // }
 
 function computeTrackingType(configuration) {
-  if (!performDraw(configuration.sampleRate)) {
+  if (!performDraw(configuration.sessionSampleRate)) {
     return LoggerTrackingType.NOT_TRACKED
   }
   return LoggerTrackingType.TRACKED
 }
 
 function computeSessionState(configuration, rawSessionType) {
-  var trackingType = hasValidLoggerSession(rawSessionType) ? rawSessionType : computeTrackingType(configuration)
+  var trackingType = hasValidLoggerSession(rawSessionType)
+    ? rawSessionType
+    : computeTrackingType(configuration)
   return {
     trackingType: trackingType,
-    isTracked: trackingType === LoggerTrackingType.TRACKED,
+    isTracked: trackingType === LoggerTrackingType.TRACKED
   }
 }
 
-function hasValidLoggerSession(trackingType){
-  return trackingType === LoggerTrackingType.NOT_TRACKED || trackingType === LoggerTrackingType.TRACKED
+function hasValidLoggerSession(trackingType) {
+  return (
+    trackingType === LoggerTrackingType.NOT_TRACKED ||
+    trackingType === LoggerTrackingType.TRACKED
+  )
 }
